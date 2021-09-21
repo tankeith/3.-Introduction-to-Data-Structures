8aff7025a1a5371b35e5b41f8a44c8cd4dafe9df
Keith Tan <24620742+tankeith@users.noreply.github.com>
2021-09-21 16:34:47 +0800

    add pointers

--- 1. Pointers and Dynamic Storage/1. pointers/pointers.cpp
+++ 1. Pointers and Dynamic Storage/1. pointers/pointers.cpp
@@ -6,13 +6,14 @@
 using namespace std;
 
 int main() {
-    int x = 100;
-    int *ptr;
-    ptr = &x;
-    cout << *ptr << endl;   // prints 100
+    int x = 100;            // initialize x with value of 100
+    int *ptr = nullptr;     // create pointer
+    ptr = &x;               // set pointer to the address of x
+    cout << *ptr << endl;   // prints 100. Not the same as cout << ptr; which gives the address of what ptr is pointing to
+    cout << ptr << endl;    // prints out address of ptr
 
-    *ptr = 20;
-    x = 40;
+    *ptr = 20;              // changes value of x to 20 via pointer
+    x = 40;                 // reassigns value to x
     cout << *ptr << endl;   // prints 40
 
     return 0;
